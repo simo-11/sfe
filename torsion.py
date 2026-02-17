19d0d7ce8f147540ea6efa96cc49d3becc6e64a1
Simo Nikula <simo.nikula@gmail.com>
2026-02-17 18:09:26 +0200

    torsion using bvp done

--- torsion.py
+++ torsion.py
@@ -19,18 +19,17 @@ Iw = 1.64e-9
 Ixx=1.14e-5
 Iyy=4.18e-5
 Ixy=1.41e-5
-L = 2
 M = 1000.0
-plt_pause=0.5
+plt_pause=0.1
 res={}
-# %% Differential equation for torsion for solve_bvp
+# Differential equation for torsion for solve_bvp
 # GI_t*theta'' - EIw*theta'''' = m
-def torsion_fun(x, y):
+def bvp_fun(x, y):
     # y[0]=theta, y[1]=theta', y[2]=theta'', y[3]=theta'''
     dydx = np.vstack((y[1], y[2], y[3], (G*It/E/Iw)*y[2]))
     return dydx
 
-def torsion_bc(ya, yb):
+def bvp_bc(ya, yb):
     # ya = value at x=0, yb = values at x=L
     return np.array([
         ya[0],        # theta(0)=0
@@ -38,34 +37,40 @@ def torsion_bc(ya, yb):
         yb[2],        # theta''(L)=0
         G*It*yb[1] - E*Iw*yb[3] - M  # moment
     ])
-# %% bvp
-x = np.linspace(0, L, 100)
-y_init = np.zeros((4, x.size))
-uc='bvp'
-res[uc]=solve_bvp(torsion_fun, torsion_bc, x, y_init)
-# %% plot
-fig,axes=plt.subplot_mosaic(
-[
-    ["rotation"],
-    ["moment"],
-]
-,num='torsion results',clear=True)
-for uc,sol in res.items():
-    ax=axes["rotation"]
-    ax.plot(sol.x, 180/np.pi*sol.y[0], label=uc)
-    ax.set_xlabel("x [m]")
-    ax.set_ylabel("θ(x) [deg]")
-    ax.legend()
+def bvp(L):
+    x = np.linspace(0, L, 100)
+    y_init = np.zeros((4, x.size))
+    uc=f'bvp@{L}'
+    res[uc]=solve_bvp(bvp_fun, bvp_bc, x, y_init)
+def plot(L):
+    fig,axes=plt.subplot_mosaic(
+    [
+        ["rotation"],
+        ["moment"],
+    ]
+    ,num=f'torsion results, L={L}',clear=True)
+    for uc,sol in res.items():
+        if not np.isclose(L,sol.x[-1]):
+            continue
+        ax=axes["rotation"]
+        ax.plot(sol.x, 180/np.pi*sol.y[0], label=uc)
+        ax.set_xlabel("x [m]")
+        ax.set_ylabel("θ(x) [deg]")
+        ax.legend()
+        plt.pause(plt_pause)
+        T = G*It*sol.y[1] - E*Iw*sol.y[3]
+        ax=axes["moment"]
+        ax.plot(sol.x, T, label=f"Total moment for {uc}")
+        ax.set_xlabel("x [m]")
+        ax.set_ylabel("Moment [Nm]")
+        T = G*It*sol.y[1]
+        ax.plot(sol.x, T, label=f"Moment from It for {uc}")
+        T = - E*Iw*sol.y[3]
+        ax.plot(sol.x, T, label=f"Moment from Iw for {uc}")
+        ax.legend()
+    fig.tight_layout()
     plt.pause(plt_pause)
-    T = G*It*sol.y[1] - E*Iw*sol.y[3]
-    ax=axes["moment"]
-    ax.plot(sol.x, T, label=f"Total moment for {uc}")
-    ax.set_xlabel("x [m]")
-    ax.set_ylabel("Moment [Nm]")
-    T = G*It*sol.y[1]
-    ax.plot(sol.x, T, label=f"Moment from It for {uc}")
-    T = - E*Iw*sol.y[3]
-    ax.plot(sol.x, T, label=f"Moment from Iw for {uc}")
-    ax.legend()
-    plt.pause(plt_pause)
-#
+# %% solve
+for L in (0.1,0.2,1,2):
+    bvp(L)
+    plot(L)
